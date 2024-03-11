15c8b4c08fe4a47bc408f36bfd05216bc124aba7
Marcos Souza <markin.mv.amaral@gmail.com>
2024-03-11 20:07:18 -0300

    Update init-aws.sh

--- config/localstack/init-aws.sh
+++ config/localstack/init-aws.sh
@@ -1,25 +1,2 @@
 #!/bin/bash
-awslocal dynamodb create-table \
-   --table-name tf-customers-table \
-   --attribute-definitions AttributeName=id,AttributeType=S \
-                           AttributeName=cpf,AttributeType=S \
-   --key-schema AttributeName=id,KeyType=HASH \
-   --provisioned-throughput ReadCapacityUnits=2,WriteCapacityUnits=2 \
-    --global-secondary-index "[
-        {
-      \"IndexName\": \"cpf\",
-      \"KeySchema\": [
-        {
-           \"AttributeName\": \"cpf\",
-          \"KeyType\": \"HASH\"
-        }
-      ],
-      \"Projection\": {
-        \"ProjectionType\": \"ALL\"
-      },
-      \"ProvisionedThroughput\": {
-        \"ReadCapacityUnits\": 1,
-        \"WriteCapacityUnits\": 1
-      }
-    }
-    ]"
+aws dynamodb create-table --endpoint-url http://localhost:4566 --table-name tf-customers-table --attribute-definitions AttributeName=cpf,AttributeType=S --key-schema AttributeName=cpf,KeyType=HASH --provisioned-throughput ReadCapacityUnits=2,WriteCapacityUnits=2
