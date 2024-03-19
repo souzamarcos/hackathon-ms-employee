a2522cabf408d88a2034374916cf6744895d1d41
Guilherme Oliveira <ghvoliveira@gmail.com>
2024-03-19 00:19:17 -0300

    Exclusão arquivos

--- usecase/src/main/java/com/fiap/burger/usecase/misc/exception/CustomerCpfAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.fiap.burger.usecase.misc.exception;
-
-public class CustomerCpfAlreadyExistsException extends DomainException {
-
-    public CustomerCpfAlreadyExistsException() {
-        super("Customer CPF already exists");
-    }
-
-}
